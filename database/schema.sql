317b1af6fc802467f277244333d8a69806f4e1e5
Natchaya-350 <67200350@kmitl.ac.th>
2026-07-29 20:45:42 +0700

    facility

--- database/schema.sql
+++ database/schema.sql
@@ -3,19 +3,34 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS dormitories (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  phone TEXT NOT NULL,
-  water_fee REAL  NOT NULL,
-  electricity_fee REAL NOT NULL,
-  monthly_rent REAL NOT NULL,
-  gender_type TEXT  NOT NULL,
-  room_type TEXT NOT NULL,
-  image_url TEXT ,
-  location_id INTEGER
+  name TEXT,
+  phone TEXT,
+  water_fee REAL,
+  electricity_fee REAL,
+  monthly_rent REAL,
+  gender_type TEXT,
+  room_type TEXT,
+  image_url TEXT,
+  location_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS amenities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR,
+  slug VARCHAR,
+  category VARCHAR,
+  icon VARCHAR,
+);
+
+CREATE TABLE IF NOT EXISTS dormitory_amenities (
+  dormitory_id INTEGER,
+  amenity_id INTEGER,
 );
 
 CREATE TABLE IF NOT EXISTS locations (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  map_url TEXT
+  map_url TEXT,
 );
+
+
 
